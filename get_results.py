af0475e0f184b690d829c4c93d27e2b7db502fef
Adesh <adeshkadambi@gmail.com>
2022-09-27 19:25:58 -0400

    clean

--- get_results.py
+++ get_results.py
@@ -112,45 +112,10 @@ if __name__ == "__main__":
                     out_filename = args.output
                 visualized_output.save(out_filename)
 
-                preds = predictions['instances'].pred_boxes.tensor, predictions['instances'].scores, predictions['instances'].pred_classes
+                preds = {
+                  'boxes': predictions['instances'].pred_boxes.tensor.cpu().numpy(),
+                  'scores': predictions['instances'].scores.cpu().numpy(),
+                  'classes': predictions['instances'].pred_classes.cpu().numpy()
+                }
                 with open(out_filename_pkl, 'wb') as file:
                   pickle.dump(preds, file)
-                
-    elif args.video_input:
-        video = cv2.VideoCapture(args.video_input)
-        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frames_per_second = video.get(cv2.CAP_PROP_FPS)
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        basename = os.path.basename(args.video_input)
-
-        if args.output:
-            if os.path.isdir(args.output):
-                output_fname = os.path.join(args.output, basename)
-                output_fname = os.path.splitext(output_fname)[0] + ".mkv"
-            else:
-                output_fname = args.output
-            assert not os.path.isfile(output_fname), output_fname
-            output_file = cv2.VideoWriter(
-                filename=output_fname,
-                # some installation of opencv may not support x264 (due to its license),
-                # you can try other format (e.g. MPEG)
-                fourcc=cv2.VideoWriter_fourcc(*"x264"),
-                fps=float(frames_per_second),
-                frameSize=(width, height),
-                isColor=True,
-            )
-        assert os.path.isfile(args.video_input)
-        for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
-            if args.output:
-                output_file.write(vis_frame)
-            else:
-                cv2.namedWindow(basename, cv2.WINDOW_NORMAL)
-                cv2.imshow(basename, vis_frame)
-                if cv2.waitKey(1) == 27:
-                    break  # esc to quit
-        video.release()
-        if args.output:
-            output_file.release()
-        else:
-            cv2.destroyAllWindows()
